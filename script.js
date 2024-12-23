b214475d461db8f8cfb8faf3047cadbc862a144b
pepeguh <absosin96@gmail.com>
2024-12-23 18:51:49 +1200

    r_2

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let valueD = 60000;
-let valueC = 0;
-let spanD = document.getElementById('dirty')
-let spanC = document.getElementById('clear')
-const ourFunc = ()=>{
-    if(valueD==0){
-        clearInterval(timer)
-        return
-    }
-    valueD = valueD - 1;
-    valueC = valueC + 1;
-    spanD.innerText=valueD;
-    spanC.innerText=valueC;
+let valueD = 59000;
+let valueC = 1000;
+let spanD = document.getElementById("dirty");
+let spanC = document.getElementById("clear");
+const startDate = new Date("2024-12-20T00:00:00");
+const today = new Date();
+const hoursElapsed = Math.floor((today - startDate) / (1000 * 60 * 60));
+valueD -= hoursElapsed;
+valueC += hoursElapsed;
+if (valueD == 0 || valueD < 0) {
+  valueD = 0;
+  valueC = 60000;
 }
-const timer = setInterval(ourFunc,200)
+spanD.innerText = valueD;
+spanC.innerText = valueC;
